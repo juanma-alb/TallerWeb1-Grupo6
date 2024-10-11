9dfcd05216984479bf404fd08dd55e5ca5e85d20
juanma-alb <168273755+juanma-alb@users.noreply.github.com>
2024-10-10 23:13:22 -0300

    Implementacion de funciones CRUD para admin pt.1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
 -- Inserta un usuario
 INSERT INTO Usuario(id, email, password, rol, activo, nombre, descripcion, ciudad, foto)
-VALUES (null, 'test@unlam.edu.ar', 'test', 'ADMIN', true, 'alguien', 'Me gusta mucho cocinar y compartir recetas con mis seres queridos', 'BuenosAires', 'default-user.png');
+VALUES (1, 'test@unlam.edu.ar', 'test', 'ADMIN', true, 'alguien', 'Me gusta mucho cocinar y compartir recetas con mis seres queridos', 'BuenosAires', 'default-user.png'),
+(null, 'test2@unlam.edu.ar', 'test2', 'USER', true, 'alguien', 'Usuario prueba', 'BuenosAires', 'default-user.png');
 
 -- Inserta recetas sin especificar el id
 INSERT INTO receta (calorias, comensales, descripcion, foto, nombre, tiempoPreparacion, calificacion, categoria, subcategoria, usuario_id)
 VALUES
-    (248, 5, '¡Te damos la mejor receta de todos los tiempos!', '/spring/imagenes/slides/slide1.jpeg','Pozole Rojo', 77, 5, 'carne', 'cerdo', null),
-    (247, 4, '¡Irresistible por donde lo veas!', '/spring/imagenes/slides/slide2.jpg', 'Pavo a la Hawaiana', 76, 4, 'caza', 'pollo', null),
-    (246, 3, 'Dulzura natural de las zanahorias', '/spring/imagenes/slides/slide3.jpg', 'Pan de zanahoria', 75, 3, 'pescado', 'salmon', null),
-    (244, 2, 'Plato tradicional de la cocina arequipeña.', '/spring/imagenes/slides/slide4.jpg', 'Ají de calabaza', 74, 2, 'pasta', 'espaguetis', null),
-    (243, 1, 'Receta con un toque especial al estilo mexicano.', '/spring/imagenes/slides/slide5.jpg', 'Bacalao a la vazcaína', 73, 1, 'cocteles', 'ponche', null),
-    (242, 5, 'Estofado japonés que combina múltiples sabores.', '/spring/imagenes/slides/slide6.jpg', 'Sukiyaki', 72, 5, 'pasta', 'pizza', null);
+    (248, 5, '¡Te damos la mejor receta de todos los tiempos!', '/spring/imagenes/slides/slide1.jpeg','Pozole Rojo', 77, 5, 'carne', 'cerdo', 1),
+    (247, 4, '¡Irresistible por donde lo veas!', '/spring/imagenes/slides/slide2.jpg', 'Pavo a la Hawaiana', 76, 4, 'caza', 'pollo', 1),
+    (246, 3, 'Dulzura natural de las zanahorias', '/spring/imagenes/slides/slide3.jpg', 'Pan de zanahoria', 75, 3, 'pescado', 'salmon', 1),
+    (244, 2, 'Plato tradicional de la cocina arequipeña.', '/spring/imagenes/slides/slide4.jpg', 'Ají de calabaza', 74, 2, 'pasta', 'espaguetis', 1),
+    (243, 1, 'Receta con un toque especial al estilo mexicano.', '/spring/imagenes/slides/slide5.jpg', 'Bacalao a la vazcaína', 73, 1, 'cocteles', 'ponche', 1),
+    (242, 5, 'Estofado japonés que combina múltiples sabores.', '/spring/imagenes/slides/slide6.jpg', 'Sukiyaki', 72, 5, 'pasta', 'pizza', 1);
